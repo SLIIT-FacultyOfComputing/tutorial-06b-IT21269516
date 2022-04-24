b4197941981b9f680f8ff0cf95d1fba3621666c4
IT21269516 <99169333+IT21269516@users.noreply.github.com>
2022-04-24 10:58:56 +0000

    Exercise 2 complete

--- Box.cpp
+++ Box.cpp
@@ -1,7 +1,26 @@
-#include "Box.h"
+#include "Box.h";
 
 // Implement setters and getters
+void Box::setLength(int len){
+  length = len;
+}
+void Box::setWidth(int wid){
+  width = wid;
+}
+void Box::setHeight(int ht){
+  height = ht;
+}     
 
+int Box::getLength(){
+  return length;
+}
+int Box::getWidth(){
+  return width;
+}
+int Box::getHeight(){
+  return height;
+}
 // Implemenet the calcVolume() unction
 int Box::calcVolume() {
+  return length * width * height;
 }
